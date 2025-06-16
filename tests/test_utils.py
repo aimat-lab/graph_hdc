56b469ab604adcf4072deacf7f9bfa2f8692a825
Jonas Teufel <jonseb1998@gmail.com>
2025-06-16 16:58:34 +0200

    changed the unittests to optionally identify/exclude the ones which require latex to be installed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -22,6 +22,7 @@ def test_get_version():
     assert version != ''
 
 
+@pytest.mark.localonly
 def test_render_latex():
     output_path = os.path.join(ASSETS_PATH, 'out.pdf')
     render_latex({'content': '$\pi = 3.141$'}, output_path)
